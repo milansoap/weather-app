9d65df719cc6fc94731c3f2d685da71b784289dc
milansoap <milan.seid123@gmail.com>
2024-09-03 16:25:12 +0200

    Adding header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  // Make the app.component.css global so we can override the body tag
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'weather-app';
